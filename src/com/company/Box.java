7137cd1f48bf77e678b047475f67cde740f1a15c
Viktoria Aaris <viktoria_aaris@hotmail.com>
2022-03-30 14:28:11 +0200

    make cell inner class and create a colony

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -1,5 +1,8 @@
 package com.company;
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 import static com.company.Window.SIZE;
 
 public class Box extends JPanel{
@@ -7,9 +10,45 @@ public class Box extends JPanel{
 
     public Box(int x, int y){
         super();
-        this.cell = new Cell();
+        cell = new Box.Cell();
         setBounds(SIZE*x, SIZE*y, SIZE, SIZE);
         setBackground(cell.getColor());
     }
 
+    public void setColor(){
+        setBackground(cell.getColor());
+    }
+
+    public class Cell {
+        private Status status;
+        private Color color;
+        private ArrayList<Cell> nearCells = new ArrayList<Cell>();
+
+        public Cell(){
+            status = Status.NONE;
+            color =  status.getColor();
+        }
+
+        public void addNearCell(Cell cell){
+            nearCells.add(cell);
+        }
+
+        public Status getStatus() {
+            return status;
+        }
+
+        public void setStatus(Status status) {
+            this.status = status;
+            color = status.getColor();
+            Box.this.setColor();
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public void setColor(Color color) {
+            this.color = color;
+        }
+    }
 }
